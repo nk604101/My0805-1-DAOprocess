25aa3d02704e8c69732ccb39806676be0920e0e5
HsinWei <fatjuan2000@hotmail.com>
2016-08-05 11:55:34 +0800

    menu setting

--- app/src/main/java/com/work/hsinwei/my0805_1_daoprocess/MainActivity.java
+++ app/src/main/java/com/work/hsinwei/my0805_1_daoprocess/MainActivity.java
@@ -3,6 +3,7 @@ package com.work.hsinwei.my0805_1_daoprocess;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -17,9 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
         //DAO
         dataInfoDAO dao =new dataInfoDAOImpl(MainActivity.this);
+        /*
         dao.addDataInfo(new dataInfo("aaa","addr1","11111"));
         dao.addDataInfo(new dataInfo("bbb","addr2","22222"));
         dao.addDataInfo(new dataInfo("ccc","addr3","33333"));
+        */
 
         ArrayList<String> display = new ArrayList<>();
         for (dataInfo data:dataInfoDAOImpl.mylist)
@@ -35,5 +38,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Log.d("T0805-DAO",""+dao.getAllDataInfo().size());
         //Log.d("T0805-DAO",""+str);
+    }
+    //建立 Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);//讀取menu 介面參數
+        return true;
     }
 }
